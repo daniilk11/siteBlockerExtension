079837fc919c19f877be0f887d6f81906efdfbfb
Daniil <daniilk11@mail.ru>
2023-11-04 11:12:58 +0100

    add delete functional

--- siteBlocker.js
+++ siteBlocker.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Получение элементов HTML
     const siteInput = document.getElementById('siteInput');
     const blockButton = document.getElementById('blockButton');
+    const unblockButton = document.getElementById('unblockButton');
     const blockedSitesList = document.getElementById('blockedSitesList');
 
     chrome.storage.local.get({ blockedSites: [] }, function(data) {
@@ -14,10 +15,10 @@ document.addEventListener('DOMContentLoaded', function() {
         const site = siteInput.value.trim();
         if (site) {
             // Добавление нового заблокированного сайта в хранилище
-            chrome.storage.local.get({ blockedSites: [] }, function(data) {
-                const blockedSites = data.blockedSites;
+            chrome.storage.local.get({blockedSites: []}, function (data) {
+                const blockedSites = data.blockedSites || [];
                 blockedSites.push(site);
-                chrome.storage.local.set({ blockedSites: blockedSites }, function() {
+                chrome.storage.local.set({blockedSites: blockedSites}, function () {
                     updateBlockedList(blockedSites);
                     siteInput.value = '';
                 });
@@ -25,11 +26,39 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    unblockButton.addEventListener('click', function () {
+        const siteToRemove = siteInput.value.trim();
+        if (siteToRemove) {
+            // Добавление нового заблокированного сайта в хранилище
+            chrome.storage.local.get({blockedSites: []}, function (data) {
+
+                // Retrieve the existing array or create an empty array if it doesn't exist
+                let currentBlockedSites = data.blockedSites || [];
+
+                // Find the index of the site in the array
+                const index = currentBlockedSites.indexOf(siteToRemove);
+
+                if (index !== -1) {
+                    // Remove the site from the array if it exists
+                    currentBlockedSites.splice(index, 1);
+                    console.log(`Site '${siteToRemove}' unblocked now `);
+                    chrome.storage.local.set({blockedSites: currentBlockedSites}, function () {
+                        updateBlockedList(currentBlockedSites);
+                        siteInput.value = '';
+                    });
+                } else {
+                    console.log(`Site '${siteToRemove}' not found in blocked sites.`);
+                }
+            });
+        }
+    });
+
+
     // html update
     function updateBlockedList(blockedSites) {
 
         blockedSitesList.innerHTML = '';
-        blockedSites.forEach(function(site) {
+        blockedSites.forEach(function (site) {
             const listItem = document.createElement('li');
             listItem.textContent = site;
             blockedSitesList.appendChild(listItem);
