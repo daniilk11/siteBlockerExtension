e57ef4a5326f174c45c06aa7832df65fe013f056
Daniil <daniilk11@mail.ru>
2023-11-04 11:12:29 +0100

    transfer timer logic to background

--- background.js
+++ background.js
@@ -6,7 +6,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
             // TODO make new .html
             chrome.tabs.update(tabId, {url: "popup.html"});
         }
-    });
+        });
     }
 });
 
@@ -14,3 +14,73 @@ function isBlocked(url, blockedSites) {
     const urlObject = new URL(url);
     return blockedSites.some(site => urlObject.hostname.includes(site));
 }
+
+// timer
+let timerInterval;
+let remainingTime = 0;
+
+
+timerInterval = setInterval(() => {
+    if (remainingTime > 0) {
+        updateTimer();
+    }
+}, 1000);
+
+function updateTimer() {
+    remainingTime--;
+
+    if (remainingTime < 0) {
+        clearInterval(timerInterval);
+        remainingTime = 0; // Reset remaining time when timer reaches zero
+    }
+
+    chrome.storage.local.set({timer: remainingTime}); // Save remaining time
+}
+
+function startTimer() {
+    if (!timerInterval && remainingTime > 0) {
+        timerInterval = setInterval(updateTimer, 1000);
+    }
+}
+
+function pauseTimer() {
+    clearInterval(timerInterval);
+    timerInterval = null;
+}
+
+function resetTimer() {
+    clearInterval(timerInterval);
+    timerInterval = null;
+    remainingTime = 0;
+    chrome.storage.local.set({timer: 0}); // Reset remaining time
+}
+
+function setTimer(time) {
+    remainingTime = time;
+    chrome.storage.local.set({timer: time}); // Save set remaining time
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === 'startTimer') {
+        startTimer();
+        sendResponse({message: 'Timer started'});
+    } else if (request.action === 'getRemainingTime') {
+        sendResponse({remainingTime});
+    } else if (request.action === 'pauseTimer') {
+        pauseTimer();
+        sendResponse({message: 'Timer paused'});
+    } else if (request.action === 'resetTimer') {
+        resetTimer();
+        sendResponse({message: 'Timer reset'});
+    } else if (request.action === 'setTimer') {
+        setTimer(request.time);
+        sendResponse({message: 'Timer set'});
+    }
+});
+
+// Retrieve the remaining time from storage when the extension is loaded
+chrome.storage.local.get(['timer'], function (data) {
+    if (data.timer) {
+        remainingTime = data.timer;
+    }
+});
--- timer.js
+++ timer.js
@@ -1,52 +1,58 @@
-let timerInterval;
-let remainingTime = 0;
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the remaining time from the background script
+    chrome.runtime.sendMessage({action: 'getRemainingTime'}, function (response) {
+        const remainingTime = response.remainingTime;
+        if (remainingTime) {
+            updateTimerDisplay(remainingTime);
+        }
+    });
 
-function updateTimer() {
+
+    document.getElementById('start').addEventListener('click', () => {
+        chrome.runtime.sendMessage({action: 'startTimer'}, function (response) {
+            console.log(response.message);
+        });
+    });
+
+    document.getElementById('pause').addEventListener('click', () => {
+        // Send a message to pause the timer if needed
+        chrome.runtime.sendMessage({action: 'pauseTimer'});
+    });
+
+    document.getElementById('reset').addEventListener('click', () => {
+        // Send a message to reset the timer if needed
+        chrome.runtime.sendMessage({action: 'resetTimer'});
+    });
+
+    document.getElementById('set').addEventListener('click', () => {
+        const setHours = parseInt(document.getElementById('setTimer').value, 10);
+        if (!isNaN(setHours)) {
+            const setTimeInSeconds = setHours * 3600;
+            updateTimerDisplay(setTimeInSeconds);
+            chrome.runtime.sendMessage({action: 'setTimer', time: setTimeInSeconds});
+        }
+    });
+});
+
+
+function updateTimerDisplay(remainingTime) {
     const hours = Math.floor(remainingTime / 3600).toString().padStart(2, '0');
     const minutes = Math.floor((remainingTime % 3600) / 60).toString().padStart(2, '0');
     const seconds = (remainingTime % 60).toString().padStart(2, '0');
     document.getElementById('timer').innerText = `Timer: ${hours}:${minutes}:${seconds}`;
-    remainingTime--;
-
-    if (remainingTime < 0) {
-        clearInterval(timerInterval);
-        document.getElementById('timer').innerText = 'Timer: 00:00:00';
-    }
-
-    // Save the remaining time to storage for future use
-    chrome.storage.local.set({timer: remainingTime});
 }
 
-function startTimer() {
-    timerInterval = setInterval(updateTimer, 1000);
+function updateTimer() {
+    chrome.runtime.sendMessage({action: 'getRemainingTime'}, function (response) {
+        const remainingTime = response.remainingTime;
+        if (remainingTime) {
+            updateTimerDisplay(remainingTime);
+        }
+    });
 }
 
-document.getElementById('start').addEventListener('click', () => {
-    startTimer();
-});
 
-document.getElementById('pause').addEventListener('click', () => {
-    clearInterval(timerInterval);
-});
+// Update the timer every second
+setInterval(updateTimer, 1000);
 
-document.getElementById('reset').addEventListener('click', () => {
-    clearInterval(timerInterval);
-    remainingTime = 0;
-    updateTimer();
-});
-
-document.getElementById('set').addEventListener('click', () => {
-    const setHours = parseInt(document.getElementById('setTimer').value, 10);
-    if (!isNaN(setHours)) {
-        remainingTime = setHours * 3600;
-        updateTimer();
-    }
-});
-
-// Retrieve the remaining time from storage when the popup opens
-chrome.storage.local.get(['timer'], function (data) {
-    if (data.timer) {
-        remainingTime = data.timer;
-        startTimer(); // Start the timer if there is remaining time saved
-    }
-});
+// Rest of your code handling button clicks and interactions...
